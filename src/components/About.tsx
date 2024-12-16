e009832df4857e035f4873cc17d78bb136146ebd
MuhammaadArsalanZahid <165092600+MuhammaadArsalanZahid@users.noreply.github.com>
2024-12-16 08:09:28 -0800

    Add files via upload

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about' className='container pt-32'>
+        <h2 className='text-4xl md:text-5xl' data-aos="zoom-in-up">About Me</h2>
+        <p className='text-gray-500 pt-4' data-aos="zoom-in-up">
+            
+            Hi, I'm Muhammad Arsalan Zahid, a student at GIAIC, persuing a course in Artificial Intelligence, Web 3.0, & Metaverse a passionate Frontend Developer with 2 years of experience building responsive, user-friendly websites and applications. My journey into web development started with a love for design and technology, and now I specialize in creating visually appealing, fast, and interactive user interfaces. I am skilled in HTML, CSS, JavaScript, and popular frontend frameworks like React.js and Vue.js. I take pride in writing clean, efficient code that ensures a seamless user experience across different devices and browsers. Whether it's turning a wireframe into a fully functional website or optimizing an app for performance, I approach each project with attention to detail and a focus on user-centric design.
+
+
+            
+        </p>
+      
+    </div>
+  )
+}
+
+export default About
